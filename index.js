007d4560bbfaa7cbe361522aa2eb0309d8c9675f
saraogiraj94 <saraogi.raj40@gmail.com>
2020-08-02 18:24:39 +0530

    Closing bridge

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Bridge = require("./lib/bridge");
 let config = require("./config.js");
 const bridge = new Bridge(config);
+//Catching error and closing the bridge
 bridge.on("error", (err) => {
   console.log(err);
   bridge.close();
