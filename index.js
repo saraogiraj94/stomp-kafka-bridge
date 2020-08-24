63efa0c10ac2aceaad91389c3c82b0a20fc9ea19
saraogiraj94 <saraogi.raj40@gmail.com>
2020-08-24 23:55:10 +0530

    EXPORTED THE LIBRARY FROM INDEX

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const Bridge = require("./lib/bridge");
-let config = require("./config.js");
-const bridge = new Bridge(config);
-//Catching error and closing the bridge
-bridge.on("error", (err) => {
-  console.log(err);
-  bridge.close();
-});
-
-bridge.run().catch(console.error);
+module.exports = require("./lib/Bridge.js");
